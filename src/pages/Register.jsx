ff7e3b5380ed42240561e1d7d85a9d5260f5ed7b
piyush singh tanwar <piyushsinghtomar777@gmail.com>
2026-03-15 13:33:27 +0530

    just some changes on userroutes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -11,7 +11,7 @@ export default function Register() {
   async function handlesubmit(e) {
     e.preventDefault();
     try {
-      const res = await axios.post(`${import.meta.env.VITE_API_URL}/users/register`,
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/users/users/register`,
         { fullname, email, password },
         { withCredentials: true }
       );
